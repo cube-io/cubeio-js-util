55a1096108327dafa285b5a9d26377eb0b5a1b78
Niels Roesen Abildgaard <na@justfone.dk>
2015-12-29 16:13:18 +0100

    Added curryMethod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     nextTick: require("./lib/nextTick.js"),
     splitCallback: require("./lib/splitCallback.js"),
-    p: require("./lib/splitCallback.js")
+    p: require("./lib/splitCallback.js"),
+    curryMethod: require("./lib/curryMethod.js")
 };
